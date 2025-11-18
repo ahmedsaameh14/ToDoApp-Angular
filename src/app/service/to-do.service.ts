29c704bb4ad9fc4cb49fabcb56b0afcea51300cb
Ahmed_Sameh <ahmedsaameh14@gmail.com>
2025-11-18 18:31:22 +0200

    Finish Service GET POST UPDATE DELETE

--- src/app/service/to-do.service.ts
+++ src/app/service/to-do.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IData } from '../interface/data';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ToDoService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  URL = "https://freeapi.miniprojectideas.com/api/JWT/"
+
+  getAllTasks(): Observable<IData[]> {
+    return this.http.get<IData[]>(this.URL + "GetAllTaskList")
+  }
+
+  createNewTasks(data: IData): Observable<IData> {
+    return this.http.post<IData>(this.URL + "CreateNewTask" , data)
+  }
+
+  updateTask(data: IData): Observable<IData>{
+    return this.http.put<IData>(this.URL + "UpdateTask" , data)
+  }
+
+  deleteTask(id: string):Observable<any>{
+    return this.http.delete(`${this.URL} DeleteTask?itemId=${id}`)
+  }
+  
 }
